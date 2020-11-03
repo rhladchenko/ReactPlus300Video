bbd35c07ff46669c483a6a8ceb9f2c5380cd3979
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-11-03 09:10:29 +0200

    Lesson-127

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import Youtube from '../apis/Youtube';
+import VideoList from './VideoList';
 
-const App = () => {
-    return <div className='app ui container'>
-        <SearchBar />
-    </div>
-};
+class App extends React.Component {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await Youtube.get('/search', {
+            params: {
+                q: term,
+            },
+        });
+
+        this.setState({ videos: response.data.items });
+    };
+
+    render() {
+        return (
+            <div className='app ui container'>
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="videos-list">
+                   {<VideoList videos={this.state.videos} />}
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
